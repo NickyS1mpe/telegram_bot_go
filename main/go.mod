3c3c3d28c4ea01f43bfe59245faa3f85a9facc3e
Rui Li <71556158+NickyS1mpe@users.noreply.github.com>
2025-01-08 17:20:58 -0800

    + Make a workspace + Add a list component for selecting chatrooms + Bugs fixed

--- main/go.mod
+++ main/go.mod
@@ -1,6 +1,8 @@
-module tele_bot_go
+module main
 
 go 1.21.5
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
@@ -11,7 +13,6 @@ require (
 	github.com/charmbracelet/x/ansi v0.4.5 // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
-	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
@@ -20,6 +21,7 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sahilm/fuzzy v0.1.1 // indirect
 	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/text v0.3.8 // indirect
--- main/main.go
+++ main/main.go
@@ -4,19 +4,57 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+	"config"
 )
 
-var bot *tgbotapi.BotAPI
-var updates tgbotapi.UpdatesChannel
+var (
+	docStyle        = lipgloss.NewStyle().Margin(1, 2)
+	quitTextStyle   = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	paginationStyle = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	bot             *tgbotapi.BotAPI
+	updates         tgbotapi.UpdatesChannel
+	bot_token       = config.BotToken
+	chatIDs         = config.Ids
+	chatId          int64
+)
+
+type model struct {
+	viewport    viewport.Model
+	messages    []string
+	textarea    textarea.Model
+	senderStyle lipgloss.Style
+	err         error
+	sub         chan telegramUpdate
+	list        list.Model
+	choice      string
+	id          string
+}
+
+type telegramUpdate struct {
+	Content string
+	Name    string
+}
+
+type item struct {
+	title string
+	id    string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.id }
+func (i item) FilterValue() string { return i.title }
 
 func main() {
 	var err error
@@ -32,20 +70,6 @@ func main() {
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Oof: %v\n", err)
 	}
-}
-
-type model struct {
-	viewport    viewport.Model
-	messages    []string
-	textarea    textarea.Model
-	senderStyle lipgloss.Style
-	err         error
-	sub         chan telegramUpdate
-}
-
-type telegramUpdate struct {
-	Content string
-	Name    string
 }
 
 func initialModel() model {
@@ -64,20 +88,36 @@ func initialModel() model {
 
 	ta.ShowLineNumbers = false
 
-	vp := viewport.New(30, 5)
+	vp := viewport.New(50, 5)
 	vp.SetContent(`Welcome to the chat room!
 Type a message and press Enter to send.`)
 
 	ta.KeyMap.InsertNewline.SetEnabled(false)
 
-	return model{
+	items := []list.Item{
+		item{title: "Raspberry Pi's", id: "2"},
+	}
+
+	for _, id := range chatIDs {
+		items = append(items, item{title: id.Title, id: fmt.Sprintf("%d", id.Id)})
+	}
+
+	m := model{
 		textarea:    ta,
 		messages:    []string{},
 		viewport:    vp,
 		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
 		err:         nil,
 		sub:         make(chan telegramUpdate),
+		list:        list.New(items, list.NewDefaultDelegate(), 0, 0),
+		choice:      "",
+		id:          "",
 	}
+
+	m.list.Title = "Select a chatroom"
+	m.list.Styles.PaginationStyle = paginationStyle
+
+	return m
 }
 
 func (m model) Init() tea.Cmd {
@@ -91,38 +131,61 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.viewport.Width = msg.Width
-		m.textarea.SetWidth(msg.Width)
+		if m.choice == "" {
+			h, v := docStyle.GetFrameSize()
+			m.list.SetSize(msg.Width-h, msg.Height-v)
+		} else {
+			m.viewport.Width = msg.Width
+			m.textarea.SetWidth(msg.Width)
+		}
 		return m, nil
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "esc", "ctrl+c":
+		case "ctrl+c":
 			// Quit.
 			fmt.Println(m.textarea.Value())
 			return m, tea.Quit
 		case "enter":
-			v := m.textarea.Value()
+			if m.choice != "" {
+				v := m.textarea.Value()
 
-			if v == "" {
-				// Don't send empty messages.
-				m.messages = append(m.messages, m.senderStyle.Render("Server: Don't send empty messages."))
+				if v == "" {
+					// Don't send empty messages.
+					m.messages = append(m.messages, m.senderStyle.Render("Server: Don't send empty messages."))
+					m.viewport.SetContent(strings.Join(m.messages, "\n"))
+					m.textarea.Reset()
+					m.viewport.GotoBottom()
+					return m, nil
+				}
+
+				m.messages = append(m.messages, m.senderStyle.Render("You: ")+v)
 				m.viewport.SetContent(strings.Join(m.messages, "\n"))
 				m.textarea.Reset()
 				m.viewport.GotoBottom()
-				return m, nil
+				telegramBotSendText(chatId, v)
+			} else {
+				i, ok := m.list.SelectedItem().(item)
+				if ok {
+					m.choice = i.title
+					m.id = i.id
+					id, err := strconv.ParseInt(i.id, 10, 64)
+					if err != nil {
+						fmt.Println("Error:", err)
+						return m, nil
+					}
+					chatId = id
+				}
 			}
-
-			m.messages = append(m.messages, m.senderStyle.Render("You: ")+v)
-			m.viewport.SetContent(strings.Join(m.messages, "\n"))
-			m.textarea.Reset()
-			m.viewport.GotoBottom()
-			telegramBotSendText(chatID, v)
 			return m, nil
 
 		default:
 			// Send all other keypresses to the textarea.
 			var cmd tea.Cmd
-			m.textarea, cmd = m.textarea.Update(msg)
+			if m.choice == "" {
+				m.list, cmd = m.list.Update(msg)
+			} else {
+				m.textarea, cmd = m.textarea.Update(msg)
+			}
 			return m, cmd
 		}
 
@@ -140,17 +203,28 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	default:
 
-		return m, nil
+		var cmd tea.Cmd
+		if m.choice == "" {
+			m.list, cmd = m.list.Update(msg)
+		} else {
+			m.textarea, cmd = m.textarea.Update(msg)
+		}
+		return m, cmd
 	}
 
 }
 
 func (m model) View() string {
-	return fmt.Sprintf(
-		"%s\n\n%s",
-		m.viewport.View(),
-		m.textarea.View(),
-	) + "\n\n"
+	switch m.choice {
+	case "":
+		return docStyle.Render(m.list.View())
+	default:
+		return fmt.Sprintf(
+			"%s\n\n%s",
+			m.viewport.View(),
+			m.textarea.View(),
+		) + "\n\n"
+	}
 }
 
 func telegramBotSendText(chatID int64, botMsg string) {
@@ -169,7 +243,7 @@ func getChannel() {
 func fetchHistory(sub chan telegramUpdate) tea.Cmd {
 	return func() tea.Msg {
 		for update := range updates {
-			if update.Message != nil && update.Message.Chat.ID == chatID {
+			if update.Message != nil && update.Message.Chat.ID == chatId {
 				sub <- telegramUpdate{update.Message.Text, update.Message.From.FirstName + " " + update.Message.From.LastName}
 			}
 		}
